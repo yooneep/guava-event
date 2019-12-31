212f7ad77c495cc1df684df3f4e8a7d101ff9f68
pangning <pangning@58.com>
2019-12-31 14:24:23 +0800

    DEV: thread pool

---
+++ src/main/java/com/example/demo/event/start10/EventBusListener.java
@@ -0,0 +1,38 @@
+/** 
+ * Project Name:guava-event 
+ * File Name:EventBusListener.java 
+ * Package Name:com.example.demo.event.start 
+ * Date:2019年12月29日下午12:58:02 
+ * Copyright (c) 2019
+ * Author: qiaozhi@58.com All Rights Reserved. 
+ * 
+ */
+package com.example.demo.event.start10;
+
+import com.google.common.eventbus.Subscribe;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Project Name:guava-event <br/>
+ * File Name:EventBusListener.java <br/>
+ * Package Name:com.example.demo.event.start <br/>
+ * Date:2019年12月29日下午1:01:45 <br/>
+ * 
+ * @author qiaozhi@58.com
+ * @version
+ * @since JDK 1.8
+ *
+ */
+public class EventBusListener {
+	private static Logger LOGGER = LoggerFactory.getLogger(EventBusListener.class);
+
+	@Subscribe
+	public void doAction(String event) {
+		LOGGER.info("doAction  :Received event [{}]", event);
+	}
+	@Subscribe
+	public void doAction1(String event) {
+		LOGGER.info("doAction1 :Received event [{}]", event);
+	}
+}
---
+++ src/main/java/com/example/demo/event/start10/EventBusStart.java
@@ -0,0 +1,43 @@
+/** 
+ * Project Name:guava-event 
+ * File Name:EventBusStart.java 
+ * Package Name:com.example.demo.event.start 
+ * Date:2019年12月29日下午12:56:58 
+ * Copyright (c) 2019
+ * Author: qiaozhi@58.com All Rights Reserved. 
+ * 
+ */
+package com.example.demo.event.start10;
+
+import com.google.common.eventbus.EventBus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ * Project Name:guava-event <br/>
+ * File Name:EventBusStart.java <br/>
+ * Package Name:com.example.demo.event.start <br/>
+ * Date:2019年12月29日下午1:01:20 <br/>
+ * 
+ * @author qiaozhi@58.com
+ * @version
+ * @since JDK 1.8
+ *
+ */
+public class EventBusStart {
+	private static final Logger LOGGER = LoggerFactory.getLogger(EventBusStart.class);
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Dispatcher.perThreadDispatchQueue
+		final EventBus eventBus = new EventBus();
+		eventBus.register(new EventBusListener());
+		LOGGER.info("EventBusStart start");
+		eventBus.post("Simple Event");
+		eventBus.post("Simple Event2");
+	}
+
+}
