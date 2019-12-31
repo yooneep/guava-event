080ab57f551d1be88668a7e5e4382192541563e6
pangning <pangning@58.com>
2019-12-31 11:08:22 +0800

    DEV: thread pool

--- src/main/java/com/example/demo/event/think/ThreadTest.java
+++ src/main/java/com/example/demo/event/think/ThreadTest.java
@@ -1,16 +1,16 @@
-/** 
- * Project Name:guava-event <br/> 
- * File Name:ThreadTest.java <br/> 
- * Package Name:com.example.demo.event.think <br/> 
- * Date:2019年12月30日下午10:29:22 <br/> 
- * Copyright (c) 2019<br/> 
- * Author: qiaozhi@58.com All Rights Reserved. <br/> 
- * 
+/**
+ * Project Name:guava-event <br/>
+ * File Name:ThreadTest.java <br/>
+ * Package Name:com.example.demo.event.think <br/>
+ * Date:2019年12月30日下午10:29:22 <br/>
+ * Copyright (c) 2019<br/>
+ * Author: qiaozhi@58.com All Rights Reserved. <br/>
  */
 package com.example.demo.event.think;
 
 import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -18,29 +18,28 @@ import java.util.concurrent.TimeUnit;
  * File Name:ThreadTest.java <br/>
  * Package Name:com.example.demo.event.think <br/>
  * Date:2019年12月30日下午10:29:22 <br/>
- * 
+ *
  * @author qiaozhi@58.com
- * @version
  * @since JDK 1.8
- * 
  */
 public class ThreadTest {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 //		Executor executor = DirectExecutor.directExecutor();
-		Executor executor = Executors.newFixedThreadPool(10);
-		for (int i = 0; i < 10; i++) {
-			executor.execute(new Runnable() {
-				public void run() {
-					System.out.println("11111111111111");
-					try {
-						TimeUnit.SECONDS.sleep(1);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			});
-		}
+        Executor executor = new ThreadPoolExecutor(10, 10,
+                0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>());
 
-	}
+        for (int i = 0; i < 10; i++) {
+            executor.execute(() -> {
+                System.out.println("11111111111111");
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            });
+        }
+
+    }
 }
